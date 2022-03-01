716753ff28eb061231f792e9420d948b49b7ccfd
arafathrummmanm <arafath.rummman@gamil.com>
2022-03-01 18:56:45 +0600

    code design update

--- phone-search.js
+++ phone-search.js
@@ -1,6 +1,6 @@
 //  Error Message Functon start
  const ErrorMessage = (parameter) =>{
-    const error =document.getElementById('errorDiv')
+    const error = document.getElementById('errorDiv')
      if(parameter == 'true'){
           error.innerHTML =`<p class ="fs-4 text-danger text-center" >No Result Found </p>`
 
@@ -19,8 +19,6 @@
     ipnutValue = inputField.value;
     inputField.value = '';
     return ipnutValue
-    
-   
 }
 // //Input value Function End
 
@@ -48,35 +46,27 @@ const searchData = (data) => {
         const showInfo = document.getElementById('showInfo') 
         showInfo.innerHTML="";
         const cardDiv = document.getElementById('cardDiv')
-        const error =document.getElementById('errorDiv')
+        const error = document.getElementById('errorDiv')
         error.innerHTML="";//clean Error Message
         cardDiv.innerHTML = "";//clean search Result
         for(data of data){
             brand = data.brand; //mobile brand name
             phoneName = data.phone_name //mobile name
             phoneImg = data.image //mobile image
-            
-            
-        
-        const div = document.createElement('div')
-        div.classList.add("col")
-        div.innerHTML = `
-        <div class="card">
-          <img src="${phoneImg}" class="card-img-top w-75 mx-auto p-2">
-          <div class="card-body">
-            <h5 class="card-title">${phoneName}</h5>
-            <p class="card-text">${brand}</p>
-            
-          </div>
-          <button id="detailsBtn" onclick="cardDetailsBtn('${data.slug}')" class="btn btn btn-success mx-auto px-5 py-2 rounded-0 rounded-bottom w-100 fw-bold">Details</button>
-        </div>`
-      cardDiv.appendChild(div)
-      
+            const div = document.createElement('div')
+            div.classList.add("col")
+            div.innerHTML = `
+                <div class="card">
+                <img src="${phoneImg}" class="card-img-top w-75 mx-auto p-2">
+                <div class="card-body">
+                    <h5 class="card-title">${phoneName}</h5>
+                    <p class="card-text">${brand}</p>
+                </div>
+                    <button id="detailsBtn" onclick="cardDetailsBtn('${data.slug}')" class="btn btn btn-success mx-auto px-5 py-2 rounded-0 rounded-bottom w-100 fw-bold">Details</button>
+                </div>`
+            cardDiv.appendChild(div)
+        }
     }
-
-
-    }
-        
 }
 //Search Data Function start
 
@@ -89,33 +79,32 @@ const cardDetailsBtn = (data) =>{
     
     const getDetailsData = (data) =>{
         const showInfo = document.getElementById('showInfo') 
-        showInfo.innerHTML = '';
+        showInfo.innerHTML = "";
         const newdiv = document.createElement('div')
-        newdiv.innerHTML = `<div class="card mb-2">
-        <img src="${data.image}" class="card-img-top w-75 mx-auto p-2">
-        <div id ="cardBody" class="card-body">
-          <h5 class="card-title">${data.name}</h5>
-          <p class="card-text">${data.brand}</p>
-          <p>Main Features</p>
-          <ul id ="mainFeatureUl">
-          </ul>
-            <p>Sensor</p>
-            <ul id="sensorUl" >
-          </ul>
-        </div>
-      </div>`
-     showInfo.appendChild(newdiv);
-      sensors(data.mainFeatures.sensors)
-      mainFeatures(data.mainFeatures)
+        newdiv.innerHTML = `
+            <div class="card mb-2">
+            <img src="${data.image}" class="card-img-top w-75 mx-auto p-2">
+            <div id ="cardBody" class="card-body">
+                <h5 class="card-title">${data.name}</h5>
+                <p class="card-text">${data.brand}</p>
+                <p>Main Features</p>
+
+                <ul id ="mainFeatureUl">
+                </ul>
+                <p>Sensor</p>
+                <ul id="sensorUl" >
+                </ul>
+            </div>
+        </div>`
+        showInfo.appendChild(newdiv);
+        sensors(data.mainFeatures.sensors)
+        mainFeatures(data.mainFeatures)
     }
-    
-
-
 }
 //  card button function End
 
 //catch sensors data Function start
-const sensors = (datas)=>{
+const sensors = (datas) => {
    const ul = document.getElementById('sensorUl')
    for(const data of datas){
     const li = document.createElement('li')
