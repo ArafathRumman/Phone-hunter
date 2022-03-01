a95f5795e07d2532599e2bc45303fc910190a235
arafathrummmanm <arafath.rummman@gamil.com>
2022-03-01 10:38:18 +0600

    button add on card and some color changes

--- phone-search.js
+++ phone-search.js
@@ -14,7 +14,8 @@
      }
 
  }
- 
+
+ //Input value Function
  const getValue = ()=> {
     const inputField = document.getElementById('inputBar')
     ipnutValue = inputField.value;
@@ -24,10 +25,12 @@
    
 }
 
+// Search Button Function
+
 document.getElementById('searchBtn').addEventListener('click',function(){
     inputFieldValue = getValue()
     if(inputFieldValue === ''){
-        cardDiv.innerHTML = "";
+        cardDiv.innerHTML = ""; //clean search Result
         ErrorMessage('false')
     }else{
         const url = `https://openapi.programming-hero.com/api/phones?search=${inputFieldValue}`
@@ -39,13 +42,13 @@ document.getElementById('searchBtn').addEventListener('click',function(){
 
 const searchData = (data) => {
     if(data.length == 0){
-        cardDiv.innerHTML = "";
+        cardDiv.innerHTML = ""; //clean search Result
         ErrorMessage('true')
     }else{
         const cardDiv = document.getElementById('cardDiv')
         const error =document.getElementById('errorDiv')
-        error.innerHTML="";
-        cardDiv.innerHTML = "";
+        error.innerHTML="";//clean Error Message
+        cardDiv.innerHTML = "";//clean search Result
         for(data of data){
             brand = data.brand; //mobile brand name
             phoneName = data.phone_name //mobile name
@@ -59,7 +62,9 @@ const searchData = (data) => {
           <div class="card-body">
             <h5 class="card-title">${phoneName}</h5>
             <p class="card-text">${brand}</p>
+            
           </div>
+          <button class="btn btn btn-success mx-auto px-5 py-2 rounded-0 rounded-bottom w-100 fw-bold">Details</button>
         </div>
       `
       cardDiv.appendChild(div)
